11ccddf5bc34736dbb62e415ba830380be2b466d
Emanuel António <109416548+Emaricarprogrammer@users.noreply.github.com>
2025-05-06 19:06:31 +0100

    Update loginLimit.ts

--- src/Utils/Limiters/loginLimit.ts
+++ src/Utils/Limiters/loginLimit.ts
@@ -5,9 +5,9 @@ import path from "path"
 
 const loginLimiter = limiter({
     limit: 3,
-    message:"Você excedeu o seu limite de tentativas de login, por favor tente novamente dentro de 15 minutos",
-    windowMs: 15 * 60 *1000,
-    handler:(req, res) =>{res.status(429).json({success: false, message:"Você excedeu o seu limite de tentativas de login, por favor tente novamente dentro de 15 minutos"})}
+    message:"Você excedeu o seu limite de tentativas de login, por favor tente novamente dentro de 10 segundos",
+    windowMs: 10 * 1000,
+    handler:(req, res) =>{res.status(429).json({success: false, message:"Você excedeu o seu limite de tentativas de login, por favor tente novamente dentro de 10 segundos"})}
 })
 
 export {loginLimiter}
